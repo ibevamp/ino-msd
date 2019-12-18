f67dfdf07f2b344edaf2208540fd6aa0b1f18b85
Rajiv Ramroop <rajiv.ramroop@gmail.com>
2019-12-18 16:48:23 -0500

    add custom popup upon submission

--- sites/all/themes/mck_know/javascript/main.js
+++ sites/all/themes/mck_know/javascript/main.js
@@ -721,3 +721,33 @@ function updateSlider(){
 
 }
 
+
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
+$(document).ready(function () {
+  var qstring = getUrlParameter('submitted');
+
+  if(qstring == "true"){
+   $.magnificPopup.open({
+    items: {
+        src: '<div id="test-popup" class="white-popup">Thank you for your Summary submission.</div>' ,
+    },
+    type: 'inline'
+      });
+ }
+
+});
+
